a9520ad44ed259f0f62308d9510b183816f44308
LONGTIMEE <armachitphol@gmail.com>
2025-02-17 18:06:06 +0700

    s

---
+++ UIElements/app/src/main/java/uielements/Database.java
@@ -0,0 +1,19 @@
+package uielements;
+
+// import java.util.HashMap;
+
+// public class Database {
+//     private static HashMap<String, User> users = new HashMap<>();
+
+//     public static boolean registerUser(String username, String password) {
+//         if (users.containsKey(username)) return false;
+//         users.put(username, new User(username, password, 0.0));
+//         return true;
+//     }
+
+//     public static User authenticate(String username, String password) {
+//         User user = users.get(username);
+//         return (user != null && user.getPassword().equals(password)) ? user : null;
+//     }
+// }
+
