2e08780da845338c795bed170c403c1533e047ae
LONGTIMEE <armachitphol@gmail.com>
2025-02-17 13:09:35 +0700

    Fianl
    ThisUI can run

---
+++ UIElements/app/src/main/java/uielements/CreatAcountsUI.java
@@ -0,0 +1,41 @@
+package uielements;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+public class CreatAcountsUI {
+    
+        public CreatAcountsUI(Stage primaryStage){
+        VBox root = new VBox(10);
+        root.setStyle("-fx-padding: 20; -fx-background-color:rgb(248, 248, 248);");
+
+        Label label1 = new Label("กรุณาสร้างรหัส");
+        Label label2 = new Label("UserName");
+        TextField usernameField = new TextField("");
+        Label label3 = new Label("PassWord");
+        TextField passwordField = new TextField("");
+        Button creatAccountsButton = new Button("ปุ่ม (Button): สร้างรหัส");     
+
+        creatAccountsButton.setOnAction(e -> new BookingUI(primaryStage));    
+
+        root.getChildren().addAll(label1,label2 , usernameField, label3, passwordField,creatAccountsButton);
+
+        Stage secondStage = new Stage();
+                Scene scene = new Scene(root, 400, 600);
+                secondStage.setTitle("UI บวก CSS");
+                secondStage.setX(primaryStage.getX() + 100);
+                secondStage.setY(primaryStage.getY() + 50);
+                secondStage.setScene(scene);
+                secondStage.show();
+
+
+    }
+
+}
---
+++ UIElements/app/src/main/java/uielements/paymentUI.java
@@ -0,0 +1,46 @@
+package uielements;
+
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class paymentUI{
+
+    public paymentUI(Stage primaryStage){
+        VBox root = new VBox(10);
+        root.setStyle("-fx-padding: 20; -fx-background-color:rgb(216, 135, 135);");
+
+        Label label = new Label("กรุณาชำระเงิน");
+        label.setStyle("-fx-font-size: 30px; -fx-text-fill: #333;");
+        Label label1 = new Label("จำนวนเงินที่ต้องชำระ");
+        label.setStyle("-fx-font-size: 25px; -fx-text-fill: #333;");
+        final  TextField priceField = new TextField("1600");
+        priceField.setStyle("-fx-border-color: #ccc; -fx-padding: 5px;");
+        Button paybutton = new Button("ชำระเงิน");
+        paybutton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-weight: bold;");
+        
+        
+        priceField.setId("priceField");
+        paybutton.setId("paybutton");
+
+        
+       
+        root.getChildren().addAll(label, label1, priceField, paybutton);
+
+        //สร้างหน้า (stage) ใหม่ โดยห่างจากหน้าแรก (+100,+50) ตาม x,y
+        Stage secondStage = new Stage();
+        Scene scene = new Scene(root, 400, 600);
+        secondStage.setTitle("UI บวก CSS");
+        secondStage.setX(primaryStage.getX() + 100);
+        secondStage.setY(primaryStage.getY() + 50);
+        secondStage.setScene(scene);
+        secondStage.show();
+    }
+
+}
